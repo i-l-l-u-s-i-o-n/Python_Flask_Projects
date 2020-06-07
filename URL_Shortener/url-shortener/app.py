b13b533f856d9a1fda5e98bc48839629cde7d6e0
Shivam1097 <shivam.dev1097@gmail.com>
2020-06-08 00:46:54 +0530

    Working

--- URL_Shortener/url-shortener/app.py
+++ URL_Shortener/url-shortener/app.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello Shivam!'
+    return render_template('home.html')
 
 
-@app.route('/about')
+@app.route('/your-url', methods=['GET', 'POST'])
 def about():
-    return "Shivam Shukla!"
+    if request.method == 'POST':
+        # If we are using get, we can use the data in the request by request.args['name']
+        # return render_template('your-url.html',code=request.args['code'])
+
+        # If we are handeling the POST request, we have to use request.form['name]
+        return render_template('your-url.html', code=request.form['code'])
+
+    else:
+        # Also do return redirect('/') but using url_for, we can directly put name.
+        # We can also had rendered the home page instead of redirecting but redirection also makes changes to the URL !!
+        return redirect(url_for(home))

263447d2fd2e89bb2e5d55745bc4a34584f54608
Shivam1097 <shivam.dev1097@gmail.com>
2020-06-10 20:15:05 +0530

    Connected database and crated route for register

--- Full_Stack_Development_with_flask/Flask Project/application/forms.py
+++ Full_Stack_Development_with_flask/Flask Project/application/forms.py
@@ -1,23 +1,37 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField , SubmitField, BooleanField
 
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from application.models import User
+
+
 
 
 # Creating forms using flask WT Forms.
 class LoginForm(FlaskForm):
     #  StringField("Email" here Email is the label for input type  email
-    email = StringField("Email",validators=[DataRequired()])
-    password = StringField("Password",validators=[DataRequired()])
+    email = StringField("Email",validators=[DataRequired(),Email()])
+    password = PasswordField("Password",validators=[DataRequired(),Length(min=6,max=15)])
     remember_me = BooleanField("Remember Me") # Creating checkbox
     # We must use function call i.e. form.submit() to submit our data
     submit = SubmitField("Login")
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField("Email",vaildators=[DataRequired()])
-    password = StringField("Password",vaildators=[DataRequired()])
-    password_confirm = StringField("Confirm Password",vaildators=[DataRequired()])
-    first_name = StringField("First Name",vaildators=[DataRequired()])
-    last_name = StringField("Last Name",vaildators=[DataRequired()])
+    email = StringField("Email",validators=[DataRequired(),Email()])
+    password = PasswordField("Password",validators=[DataRequired(),Length(min=6,max=15)])
+    password_confirm = PasswordField("Confirm Password",validators=[DataRequired(),Length(min=6,max=15),EqualTo('password')])
+    first_name = StringField("First Name",validators=[DataRequired(),Length(min=2,max=55)])
+    last_name = StringField("Last Name",validators=[DataRequired(),Length(min=2,max=55)])
     submit = SubmitField("Register Now")
+
+    # We must put field name i.e. email after 'validate_' to set the function name it will automatically validate that field
+    def validate_email(self,email):
+
+        # fetching record from database
+        user = User.objects(email=email.data).first()
+        if user:
+            # Following will raise the error in the  view in which this email field is used i.e the register.html page
+            raise ValidationError("Email is already in use. Pick another one.")
+
+
--- Full_Stack_Development_with_flask/Flask Project/application/routes.py
+++ Full_Stack_Development_with_flask/Flask Project/application/routes.py
@@ -25,9 +25,20 @@ def login():
 
     # Validation
     if form.validate_on_submit():
-        if request.form['email'] =='shivam@uta.com':
-            flash("You are successfully logged in!","success")
-            return redirect(url_for('index'))
+        # email = request.form['email']
+        #OR
+        email       = form.email.data
+        password    = form.password.data
+        print("Entered : ",email)
+
+        user = User.objects(email=email).first()
+
+        print("Fetched: ",user.get_password(password))
+
+        # Checking if user is registered or not and if password matched or not
+        if user and user.get_password(password):
+                flash(f"{user.first_name} You are successfully logged in!","success")
+                return redirect(url_for('index'))
         else:
             flash("You're not registered!","danger")
     return render_template('login.html',title="Login", form=form, login=True)
@@ -40,9 +51,36 @@ def courses(term="Spring 2020"):
     return render_template('courses.html', courseData=courseData, term=term, courses=True)
 
 
-@app.route('/register')
+@app.route('/register',methods=['get','post'])
 def register():
-    return render_template('register.html', register=True)
+
+    form = RegistrationForm()
+
+    # Validate_on_submit will automatically call teh validate_email method in Registration_form class and check if mail already exist  or not.
+    if form.validate_on_submit():
+        # getting total no. of documents in user collection
+        user_id = User.objects.count()
+
+        # incrementing th user id by 1 as user id needs to be unique
+        user_id +=1
+
+        email = form.email.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        print(email)
+        print(password)
+        print(first_name)
+        print(first_name)
+
+
+        user = User(user_id=user_id,email=email, first_name=first_name,last_name=last_name)
+        user.set_password(password) # Saving password after hashing
+        user.save()
+        flash("You are successfully registered!", "success")
+        return redirect(url_for('index'))
+        
+    return render_template('register.html',title="Register",form=form, register=True)
 
 
 @app.route('/enrollment', methods=['POST'])
